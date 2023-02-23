a3f8bb387e9b787c7d0c9557cb856862cba9174f
Qionn <wullaert.liam@outlook.com>
2023-02-23 15:15:16 +0100

    project setup

---
+++ engine/source/stdafx.h
@@ -0,0 +1,16 @@
+#ifndef __BT_ENGINE_STDAFX_H__
+#define __BT_ENGINE_STDAFX_H__
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#include <windows.h>
+#include <windowsx.h>
+
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_win32.h>
+
+#include <string>
+#include <vector>
+
+#endif // !__BT_ENGINE_STDAFX_H__
